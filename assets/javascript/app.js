b9d19d56b9685433df5257ac0aecdb95abe379e1
Bill Kamberos <bill.kamberos@gmail.com>
2020-01-27 12:51:06 -0500

    More styling finished - array or objects for questions added

---
+++ assets/javascript/app.js
@@ -0,0 +1,254 @@
+let game = [
+  {
+    question: "What was the name of Bart's elephant?",
+    answerA: 'Stompy',
+    answerB: 'Stampy',
+    answerC: 'Squishy',
+    answerD: 'Dumbo',
+    correctIMG: 'https://media.giphy.com/media/xT5LMIOyAHyqSFYPMA/giphy.gif',
+    correct: 'b',
+    correctDisplay: 'Stampy'
+  },
+  {
+    question:
+      "What was the name of Ned Flanders' store in the Springfield Mall?",
+    answerA: 'The Leftorium',
+    answerB: "Lefties R' Us",
+    answerC: "Devil's Hand Emporium",
+    answerD: 'Second Hand Store',
+    correctIMG: 'https://media.giphy.com/media/3orif8DzFlLlGe00M0/giphy.gif',
+    correct: 'a',
+    correctDisplay: 'The Leftorium'
+  },
+  {
+    question: "Who is Bart Simpson's nemesis?",
+    answerA: 'Groundskeeper Willie',
+    answerB: 'Side Show Mel',
+    answerC: 'Chief Wiggum ',
+    answerD: 'Side Show Bob',
+    correctIMG: 'https://media.giphy.com/media/RSOUOj8H9A3Xq/giphy.gif',
+    correct: 'd',
+    correctDisplay: 'Side Show Bob'
+  },
+  {
+    question: "Krusty The Clown's real name is...",
+    answerA: 'Rory B Bellows',
+    answerB: 'Herschel Krustofsky',
+    answerC: 'Christopher Krust',
+    answerD: 'Homer Simpson',
+    correctIMG: 'https://media.giphy.com/media/3o6MbaB2rYRC7R3Ng4/giphy.gif',
+    correct: 'b',
+    correctDisplay: 'Herschel Krustofsky'
+  },
+  {
+    question: 'What instrument does Lisa Simpson play?',
+    answerA: 'Flute',
+    answerB: 'Drums',
+    answerC: 'Saxophone',
+    answerD: 'Trumpet',
+    correctIMG: 'https://media.giphy.com/media/l2Je0GacMKhoqIM36/giphy.gif',
+    correct: 'c',
+    correctDisplay: 'Saxophone'
+  },
+  {
+    question: "Marge Simpson's maiden name is..",
+    answerA: 'Bouvier',
+    answerB: 'Simmons',
+    answerC: 'Walters',
+    answerD: 'Wiggum',
+    correctIMG: 'https://media.giphy.com/media/l2Jedx38sk0Xclaxy/giphy.gif',
+    correct: 'a',
+    correctDisplay: 'Bouvier'
+  },
+  {
+    question: "What was the name of Homer's plow business?",
+    answerA: 'Plow King',
+    answerB: 'Super Plow',
+    answerC: 'Plows For Less',
+    answerD: 'Mr. Plow',
+    correctIMG: 'https://media.giphy.com/media/3o75218xzv3Hoe66go/giphy.gif',
+    correct: 'd',
+    correctDisplay: 'Mr. Plow'
+  },
+  {
+    question:
+      "Name the drink that Homer invented using children's cough syrup.",
+    answerA: 'Flaming Homer',
+    answerB: "Homer's Special Sauce",
+    answerC: 'Rum and Homer',
+    answerD: 'Long Homer Iced Tea',
+    correctIMG: 'https://media.giphy.com/media/8S25zvJxAgN7G/giphy.gif',
+    correct: 'a',
+    correctDisplay: 'Flaming Homer'
+  },
+  {
+    question: "Maggie's first word was?",
+    answerA: 'Homer',
+    answerB: 'Mama',
+    answerC: 'Bart',
+    answerD: 'David Hasselhoff',
+    correctIMG: 'https://media.giphy.com/media/3orifhQjTT1CgRE47K/giphy.gif',
+    correct: 'c',
+    correctDisplay: 'Bart'
+  },
+  {
+    question: "The Simpson's street address is..",
+    answerA: '742 Evergreen Terrace',
+    answerB: '768 Shelbyville Way',
+    answerC: '31 Spooner St',
+    answerD: '1024 Cherry Street',
+    correctIMG: 'https://media.giphy.com/media/3orieMaXYViCoYIrny/giphy.gif',
+    correct: 'a',
+    correctDisplay: '742 Evergreen Terrace'
+  },
+  {
+    question: 'Who shot Mr. Burns?',
+    answerA: 'Maggie Simpson',
+    answerB: 'Waylon Smithers',
+    answerC: 'Chief Wiggum',
+    answerD: 'Homer Simpson',
+    correctIMG: 'https://media.giphy.com/media/3o6MbolBsP7J5X43lK/giphy.gif',
+    correct: 'a',
+    correctDisplay: 'Maggie Simpson'
+  },
+  {
+    question: "Homer's quartet had a hit song named...",
+    answerA: "Don't Stop The Lovin'",
+    answerB: 'Baby Forever',
+    answerC: 'The Long Walk',
+    answerD: 'Baby On Board',
+    correctIMG: 'https://media.giphy.com/media/NNvShpev4l7he/giphy.gif',
+    correct: 'd',
+    correctDisplay: 'Baby On Board'
+  },
+  {
+    question: "How do you spell Apu's last name?",
+    answerA: 'Nahasapeemapetilon',
+    answerB: 'Nahasapennapetilon',
+    answerC: 'Nuhhasapeenapetilon',
+    answerD: 'Nuhasapeemapedilon',
+    correctIMG: 'https://media.giphy.com/media/3o6Mb43ToTpO3WO4Cc/giphy.gif',
+    correct: 'a',
+    correctDisplay: 'Nahasapeemapetilon'
+  },
+  {
+    question: "What did the Simpson's get in their first Christmas episode?",
+    answerA: 'A Cat',
+    answerB: 'Their Car',
+    answerC: 'A Dog',
+    answerD: 'Their House',
+    correctIMG: 'https://media.giphy.com/media/xT5LMvmOApGsQDXGdq/giphy.gif',
+    correct: 'c',
+    correctDisplay: 'A Dog'
+  },
+  {
+    question: "What is the name of Bart's favorite comic book action hero?",
+    answerA: 'Radiation Dude',
+    answerB: 'Gamma Ray',
+    answerC: 'Atom Bomb',
+    answerD: 'Radioactive Man',
+    correctIMG: 'https://media.giphy.com/media/3orif1uDmLAeKeJPkA/giphy.gif',
+    correct: 'd',
+    correctDisplay: 'Radioactive Man'
+  },
+  {
+    question: 'Where did Homer get the idea for his new name, Max Power?',
+    answerA: 'A Hair Dryer',
+    answerB: 'An Adult Film',
+    answerC: 'A Superhero',
+    answerD: 'From A Friend',
+    correctIMG: 'https://media.giphy.com/media/3oriePDp0hGGwvwc1y/giphy.gif',
+    correct: 'a',
+    correctDisplay: 'A Hair Dryer'
+  },
+  {
+    question: 'Who moved in across the street from the Simpsons?',
+    answerA: 'Comic Book Guy',
+    answerB: 'Bill Clinton',
+    answerC: 'George Bush Sr',
+    answerD: 'Ronald Reagan',
+    correctIMG: 'https://media.giphy.com/media/xT5LMResTV3RqAnz1K/giphy.gif',
+    correct: 'c',
+    correctDisplay: 'George Bush Sr'
+  },
+  {
+    question: 'What section of the power plant does Homer work in?',
+    answerA: 'Sector 22',
+    answerB: 'Sector 7G',
+    answerC: 'Sector 8F',
+    answerD: 'Sector 39',
+    correctIMG: 'https://media.giphy.com/media/KYMR9VsDGs3PW/giphy.gif',
+    correct: 'b',
+    correctDisplay: 'Sector 7G'
+  },
+  {
+    question: "What does the J in Homer's middle name stand for?",
+    answerA: 'Jason',
+    answerB: 'Jojo',
+    answerC: 'Jacob',
+    answerD: 'Jay',
+    correctIMG: 'https://media.giphy.com/media/xT5LMxABbTX8yQA6Y0/giphy.gif',
+    correct: 'd',
+    correctDisplay: 'Jay'
+  },
+  {
+    question: "What's Patty and Selma's favourite TV show?",
+    answerA: 'MacGyver',
+    answerB: 'Matlock',
+    answerC: 'Murder She Wrote',
+    answerD: 'Golden Girls',
+    correctIMG: 'https://media.giphy.com/media/xT5LML8JpJ6dnfv4HK/giphy.gif',
+    correct: 'a',
+    correctDisplay: 'MacGyver'
+  },
+  {
+    question: 'How many kids did Apu and Manjula give birth to?',
+    answerA: '7',
+    answerB: '8',
+    answerC: '10',
+    answerD: '4',
+    correctIMG: 'https://media.giphy.com/media/3orieQFKy190vYgEes/giphy.gif',
+    correct: 'b',
+    correctDisplay: '8'
+  },
+  {
+    question: 'Who founded the Simpsons’ hometown?',
+    answerA: 'Jebadiah Springfield',
+    answerB: 'Herbert Shelbyville',
+    answerC: 'Cletus Spuckler',
+    answerD: 'Lyle Lanley',
+    correctIMG: 'https://media.giphy.com/media/3o6Mbal7VJJo0rtslO/giphy.gif',
+    correct: 'a',
+    correctDisplay: 'Jebadiah Springfield'
+  },
+  {
+    question: 'Who is the spokesperson for Duff Beer?',
+    answerA: 'Duffmeister',
+    answerB: 'Duff The Magic Dragon',
+    answerC: 'Duffman',
+    answerD: 'Spuds Mackenzie',
+    correctIMG: 'https://media.giphy.com/media/3oriePn1Zovcmb7q5G/giphy.gif',
+    correct: 'c',
+    correctDisplay: 'Duffman'
+  },
+  {
+    question: "What is Homer's catch phrase?",
+    answerA: '¡Ay, Caramba!',
+    answerB: "D'oh!",
+    answerC: 'Okalie Dokalie',
+    answerD: "Dat's Crayy",
+    correctIMG: 'https://media.giphy.com/media/3o6MbkON1CxTbqPT56/giphy.gif',
+    correct: 'b',
+    correctDisplay: "D'oh!"
+  },
+  {
+    question: 'Whats is the name of the Simpsons yearly halloween special?',
+    answerA: 'Treehouse of Horror',
+    answerB: 'House of Terror',
+    answerC: 'Simpsons Scartacular',
+    answerD: "Springfield's Sacrifice",
+    correctIMG: 'https://media.giphy.com/media/rBstaErKItWKs/giphy.gif',
+    correct: 'a',
+    correctDisplay: 'Treehouse of Horror'
+  }
+];
